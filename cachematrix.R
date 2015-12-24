269da6380167d4d5bb5915951a246c1f435b19b7
ModernCan <shervin.khazaeli@yahoo.com>
2015-12-23 21:28:52 -0500

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching The Inverse of a Square Matrix:
+## The objective of the following function is to cache the 
+## inverse of a square matrix instead of computing it repeatedly.
+## To this end, two functions are presented: create a matrix
+## which the inverse can be cached, and compute the inverse of 
+## a matrix in a way that if the inverse has been calculate, it
+## retrives the inverse from cache.
 
-## Write a short comment describing this function
+## Creating the special MATRIX object, which the inverse can be cached
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        invse <- NULL
+        
+        set <- function(y){
+                x <<- y
+                invse <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inverse) invse <<- inverse
+        getinverse <- function() invse
+        
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Computing the inverse of a matrix such a way that if the inverse has been
+## calculated, the function retrive the inverse from cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        invse <- x$getinverse()
+        
+        if (!is.null(invse)) {
+                message("Getting Cached Data ...")
+                return(invse)
+        }
+        
+        m <- x$get()
+        invse <- solve(m, ...)
+        x$setinverse(invse)
+        
+        invse
 }
